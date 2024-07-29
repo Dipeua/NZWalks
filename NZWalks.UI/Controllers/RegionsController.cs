6051f095b80856feb61515aadc5602daf3c2b955
Dipeua <dipeuaberthold@gmail.com>
2024-07-29 12:05:37 +0200

    POST

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NZWalks.UI.Models;
+using System.Text;
+using System.Text.Json;
 
 namespace NZWalks.UI.Controllers
 {
-    public class RegionsController(IHttpClientFactory _httpClientFactory) : Controller
+    public class RegionsController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string _url;
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _url = "https://localhost:7104/api/regions";
+        }
         // GET
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDto> responseBody = new List<RegionDto>();
@@ -14,7 +24,7 @@ namespace NZWalks.UI.Controllers
             {
                 // Get api information
                 var client = _httpClientFactory.CreateClient();
-                var httpResponseMessage = await client.GetAsync("https://localhost:7104/api/regions");
+                var httpResponseMessage = await client.GetAsync(_url);
                 
                 // check if it's OK!
                 httpResponseMessage.EnsureSuccessStatusCode();
@@ -28,5 +38,37 @@ namespace NZWalks.UI.Controllers
             }
             return View(responseBody);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> Create(AddRegion addRegion)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(_url),
+                Content = new StringContent(JsonSerializer.Serialize(addRegion), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var responseBody = await httpRequestMessage.Content.ReadFromJsonAsync<AddRegion>();
+            
+            if(responseBody is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+
+            return View(responseBody);
+        }
+
     }
 }
