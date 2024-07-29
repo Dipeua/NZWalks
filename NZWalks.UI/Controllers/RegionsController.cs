984021c807e2155647d3e99dbc2ff4eef3982c56
Dipeua <dipeuaberthold@gmail.com>
2024-07-29 14:12:33 +0200

    GET -Id

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NZWalks.UI.Models;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 
@@ -18,18 +19,25 @@ namespace NZWalks.UI.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            List<RegionDto> responseBody = new List<RegionDto>();
+            var responseBody = new List<RegionDto>();
 
             try
             {
                 // Get api information
                 var client = _httpClientFactory.CreateClient();
-                var httpResponseMessage = await client.GetAsync(_url);
-                
-                // check if it's OK!
-                httpResponseMessage.EnsureSuccessStatusCode();
+                //var httpResponseMessage = await client.GetAsync(_url);
 
-                responseBody.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<List<RegionDto>>());
+                //// check if it's OK!
+                //httpResponseMessage.EnsureSuccessStatusCode();
+
+                //responseBody.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<List<RegionDto>>());
+
+                var response = await client.GetFromJsonAsync<List<RegionDto>>($"{_url}");
+
+                if (response is not null)
+                {
+                    return View(response);
+                }
 
             }
             catch(Exception ex)
@@ -44,6 +52,29 @@ namespace NZWalks.UI.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid Id)
+        {
+            // Get api information
+            var client = _httpClientFactory.CreateClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<RegionDto>($"{_url}/{Id.ToString()}");
+                if (response == null)
+                {
+                    return NotFound();
+                }
+                return View(response);
+            }
+            catch (HttpRequestException ex)
+            {
+                // Log the exception or handle it as necessary
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the server.");
+            }
+        }
+
 
         // POST
         [HttpPost]
@@ -70,5 +101,28 @@ namespace NZWalks.UI.Controllers
             return View(responseBody);
         }
 
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(AddRegion addRegion)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var httpRequestMessage = new HttpRequestMessage()
+        //    {
+        //        Method = HttpMethod.Put,
+        //        RequestUri = new Uri(_url),
+        //        Content = new StringContent(JsonSerializer.Serialize(addRegion), Encoding.UTF8, "application/json")
+        //    };
+        //    var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+        //    httpResponseMessage.EnsureSuccessStatusCode();
+
+        //    var responseBody = await httpRequestMessage.Content.ReadFromJsonAsync<AddRegion>();
+
+        //    if (responseBody is not null)
+        //    {
+        //        return RedirectToAction("Index", "Regions");
+        //    }
+
+        //    return View(responseBody);
+        //}
     }
 }
